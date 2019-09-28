cde2f5c736f8dffb98d10413974fa8aa58e43c78
RyotoKubo <sax.guiter@icloud.com>
2019-09-28 22:36:21 +0900

    test

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
                   Container(
                     color: Color.fromRGBO(255, 255, 0, 0.5),
                     width: 100,
-                    height: 100,
+                    height: 200,
                   ),
                 ],
               ),
