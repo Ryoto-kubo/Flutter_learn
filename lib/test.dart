2529074b4fe26c9bfa5bddcf7a3ae7940bf9e503
RyotoKubo <sax.guiter@icloud.com>
2019-09-28 22:43:02 +0900

    test

---
+++ lib/test.dart
@@ -0,0 +1,98 @@
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(MyApp());
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         backgroundColor: Colors.teal,
+//         // appBar: AppBar(
+//         //   title: Text('hello data'),
+//         //   backgroundColor: Colors.blue[900],
+//         // ),
+//         body: SafeArea(
+//           child: Row(
+//             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//             children: <Widget>[
+//               Container(
+//                 color:Colors.red,
+//                 width: 100.0,
+//                 height: double.infinity,
+//               ),
+//               Column(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: <Widget>[
+//                   Container(
+//                     color: Colors.yellow,
+//                     width: 100,
+//                     height: 100,
+//                   ),
+//                   Container(
+//                     color: Color.fromRGBO(255, 255, 0, 0.5),
+//                     width: 100,
+//                     height: 200,
+//                   ),
+//                 ],
+//               ),
+//               Container(
+//                 padding:EdgeInsets.only(top: 50),
+//                 color:Colors.blue,
+//                 width: 100.0,
+//                 height: double.infinity,
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// // class MyHomePage extends StatefulWidget {
+// //   MyHomePage({Key key, this.title}) : super(key: key);
+
+// //   final String title;
+
+// //   @override
+// //   _MyHomePageState createState() => _MyHomePageState();
+// // }
+
+// // class _MyHomePageState extends State<MyHomePage> {
+// //   int _counter = 0;
+
+// //   void _incrementCounter() {
+// //     setState(() {
+// //       _counter++;
+// //     });
+// //   }
+
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return Scaffold(
+// //       appBar: AppBar(
+// //         title: Text(widget.title),
+// //       ),
+// //       body: Center(
+// //         child: Column(
+// //           mainAxisAlignment: MainAxisAlignment.center,
+// //           children: <Widget>[
+// //             Text(
+// //               'You have pushed the button this many times:',
+// //             ),
+// //             Text(
+// //               '$_counter',
+// //               style: Theme.of(context).textTheme.display1,
+// //             ),
+// //           ],
+// //         ),
+// //       ),
+// //       floatingActionButton: FloatingActionButton(
+// //         onPressed: _incrementCounter,
+// //         tooltip: 'Increment',
+// //         child: Icon(Icons.add),
+// //       ), // This trailing comma makes auto-formatting nicer for build methods.
+// //     );
+// //   }
+// // }
