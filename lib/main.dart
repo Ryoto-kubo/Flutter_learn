dfae3d509f259af81d24230c0cda6f8cbf2b8cee
RyotoKubo <sax.guiter@icloud.com>
2019-10-01 00:12:47 +0900

    learn #5

--- lib/main.dart
+++ lib/main.dart
@@ -6,139 +6,35 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blue,
-        body: SafeArea(
-          child: Column( // RowClassの後はchildrenを作る
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/user.jpg'),
-              ),
-              Text(
-                ('Ryoto'),
-                style:TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
-                )
-              ),
-              Text(
-                ('Flutter Callsystem'),
-                style:TextStyle(
-                  fontFamily: 'SourceSansPro',
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.0,
-                ),
-              ),
-              Container(
-                color: Colors.white,
-                margin:EdgeInsets.symmetric(vertical: 20.0, horizontal: 500.0),
-                padding: EdgeInsets.all(10.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.black
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      ('090-1234-5678'),
-                      style:TextStyle(
-                        fontFamily: 'SourceSansPro',
-                        color: Colors.black,
-                        fontSize: 20.0,
-                      )
-                    ),
-                  ],
-                )
-              ),
-              Container(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(vertical: 20.0, horizontal: 500.0),
-                padding: EdgeInsets.all(10.0),
-                // decoration: BoxDecoration(
-                //   borderRadius: BorderRadius.only(
-                //     topLeft:  const  Radius.circular(40.0),
-                //     topRight: const  Radius.circular(40.0))
-                // ),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      color: Colors.black,
-                      
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      ('email@cloud.com'),
-                      style: TextStyle(
-                        fontFamily: 'SourceSansPro',
-                        color: Colors.black,
-                        fontSize: 20.0,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: BallPage(),
     );
   }
 }
 
-// class MyHomePage extends StatefulWidget {
-//   MyHomePage({Key key, this.title}) : super(key: key);
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        title: Text('Ask me Anything'),
+        backgroundColor: Colors.blue.shade900,
+      ),
+      body: Ball(),
+    );
+  }
+}
 
-//   final String title;
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
 
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.display1,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('images/ball/ball1.png'),
+    );
+  }
+}
